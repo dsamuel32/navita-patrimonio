049af49b9c9b14534131e5210fdd594376dba856
dsamuel32 <diego.samuel.alves@gmail.com>
2020-06-22 16:44:40 -0300

    Criando readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# navita-patrimonio
+# Teste Navita Controle de Patrimônio
+
+Tecnologias Utilizadas
+- Spring Boot
+- Spring Security
+- Spring Security OAuth
+- Spring MVC
+- Spring Data JPA
+- Liquibase
+- Swagger
+- Postgresql
+- Docker
+
+## Instruções
+
+#### Executar o comando para criar o container do Postgresql
+
+```
+docker-compose up
+```
+
+#### Executar o build do projeto
+
+```
+mvn clean package
+```
+
+- Endpoint de autentição
+```
+curl -X POST -vu testjwtclientid:XY7kmzoNzl100  http://localhost:8080/patrimonio/api/oauth/token  -H "Accept: application/json" -d grant_type=password -d username=teste@teste.com -d password=123
+```
+
+- Use o access_token para se autenticar
+```
+{
+	"access_token":"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsidGVzdGp3dHJlc291cmNlaWQiXSwidXNlcl9uYW1lIjoidGVzdGUiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTI4Mjg4MzczLCJhdXRob3JpdGllcyI6WyJBRE0iXSwianRpIjoiMDEyZGNhMDMtMGYxNy00YzE4LWE1Y2QtMTU5MmFlNWRmNGNkIiwiY2xpZW50X2lkIjoidGVzdGp3dGNsaWVudGlkIn0.gAKUbBpHw_kkntUE2D2IB_De_usekG8KbTkc-7MHNKE",
+	"token_type":"bearer",
+	"refresh_token": "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOlsidGVzdGp3dHJlc291cmNlaWQiXSwidXNlcl9uYW1lIjoidGVzdGUiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiYXRpIjoiYTVjZGYzMmQtOTY4YS00YjI5LWI3ODctZjlmNTNkZTJjY2IxIiwiZXhwIjoxNTMwOTE4OTc3LCJhdXRob3JpdGllcyI6WyJBRE0iXSwianRpIjoiMDA0NTJlNzItYjBiMi00M2U0LWJjZTAtOGQ1OGUwZTU5OTg2IiwiY2xpZW50X2lkIjoidGVzdGp3dGNsaWVudGlkIn0.DFX0xw-Wa_mgRnsHuFikIrQugyipc4HAMhrrWVySJEc",
+	"expires_in":43199,
+	"scope":"read write",
+	"jti":"012dca03-0f17-4c18-a5cd-1592ae5df4cd"
+}
+```
---
+++ src/test/java/br/com/navita/patrimonio/service/impl/UsuarioServiceImplTest.java
@@ -0,0 +1,139 @@
+package br.com.navita.patrimonio.service.impl;
+
+import br.com.navita.patrimonio.dominio.builder.UsuarioBuilder;
+import br.com.navita.patrimonio.dominio.builder.UsuarioDTOBuilder;
+import br.com.navita.patrimonio.dominio.dto.*;
+import br.com.navita.patrimonio.dominio.entidade.Permissao;
+import br.com.navita.patrimonio.dominio.entidade.Usuario;
+import br.com.navita.patrimonio.repository.UsuarioRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UsuarioServiceImplTest {
+
+    @InjectMocks
+    private UsuarioServiceImpl usuarioService;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Mock
+    private Usuario usuario;
+
+    @Mock
+    private Permissao permissao;
+
+    @Mock
+    private Page<Usuario> page;
+
+    @Mock
+    private Pageable pageable;
+
+    private final Long ID = 1L;
+    private final String PERMISSAO = "ADM";
+    private final String NOME = "TESTE";
+    private final String EMAIL = "TESTE@TESTE.COM";
+
+
+    @Before
+    public void setUp() {
+        when(this.permissao.getId()).thenReturn(ID);
+        when(this.permissao.getDescricao()).thenReturn(PERMISSAO);
+        when(this.usuario.getId()).thenReturn(ID);
+        when(this.usuario.getNome()).thenReturn(NOME);
+        when(this.usuario.getEmail()).thenReturn(EMAIL);
+        when(this.usuario.getPermissoes()).thenReturn(new HashSet<>(Arrays.asList(this.permissao)));
+        when(this.pageable.getPageNumber()).thenReturn(0);
+        when(this.page.getPageable()).thenReturn(this.pageable);
+        when(this.page.getTotalElements()).thenReturn(1L);
+        when(this.page.getContent()).thenReturn(Arrays.asList(this.usuario));
+        when(this.usuarioRepository.findByLogin(anyString())).thenReturn(this.usuario);
+        when(this.usuarioRepository.findById(anyLong())).thenReturn(Optional.of(this.usuario));
+        when(this.usuarioRepository.recuperar(anyLong(),anyString(), anyString(), any(Pageable.class))).thenReturn(this.page);
+        when(this.usuarioRepository.save(any(Usuario.class))).thenReturn(this.usuario);
+
+    }
+
+    @Test
+    public void loadUserByUsername() {
+        Usuario usuario = (Usuario) this.usuarioService.loadUserByUsername(EMAIL);
+        assertEquals(ID, usuario.getId());
+        assertEquals(NOME, usuario.getNome());
+        assertEquals(EMAIL, usuario.getEmail());
+        usuario.getPermissoes().forEach(it -> {
+            assertEquals(ID, it.getId());
+            assertEquals(PERMISSAO, it.getDescricao());
+        });
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void loadUserByUsernameUsernameNotFoundException() {
+        when(this.usuarioRepository.findByLogin(anyString())).thenReturn(null);
+        this.usuarioService.loadUserByUsername(EMAIL);
+    }
+
+    @Test
+    public void getUsuarioLogado() {
+    }
+
+    @Test
+    public void recuperar() {
+        FiltroUsuarioDTO filtroUsuarioDTO = new FiltroUsuarioDTO();
+        filtroUsuarioDTO.setId(ID);
+        filtroUsuarioDTO.setNome(NOME);
+        filtroUsuarioDTO.setEmail(EMAIL);
+        PaginacaoDTO<UsuarioDTO> paginacaoDTO = this.usuarioService.recuperar(filtroUsuarioDTO);
+        assertEquals(new Integer(0), paginacaoDTO.getPagina());
+        assertEquals(new Long(1), paginacaoDTO.getTotal());
+        paginacaoDTO.getConteudo().forEach(it -> {
+            assertEquals(ID, it.getId());
+            assertEquals(NOME, it.getNome());
+            assertEquals(EMAIL, it.getEmail());
+        });
+    }
+
+    @Test
+    public void salvar() {
+        UsuarioDTO usuarioDTO = UsuarioDTOBuilder.getInstance()
+                                                 .id(ID)
+                                                 .nome(NOME)
+                                                 .email(EMAIL)
+                                                 .permissoes(new HashSet<>(Arrays.asList(new Permissao(ID, PERMISSAO))))
+                                                 .build();
+        UsuarioDTO usuarioDTOSalvo = this.usuarioService.salvar(usuarioDTO);
+        assertEquals(ID, usuarioDTOSalvo.getId());
+        assertEquals(NOME, usuarioDTOSalvo.getNome());
+        assertEquals(EMAIL, usuarioDTOSalvo.getEmail());
+        usuarioDTOSalvo.getPermissoes().forEach(it -> {
+            assertEquals(ID, it.getId());
+            assertEquals(PERMISSAO, it.getDescricao());
+        });
+
+    }
+
+    @Test
+    public void desativar() {
+        RespostaDTO respostaDTO = this.usuarioService.desativar(ID);
+        assertEquals("Usuário desativado com sucesso.", respostaDTO.getMensagem());
+    }
+}
