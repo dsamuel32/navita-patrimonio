713f6d3befb825bc022685ff88b80aa38f1bda79
dsamuel32 <diego.samuel.alves@gmail.com>
2020-06-23 15:11:22 -0300

    removendo autenticação salvar usuario para verbo post.

--- src/main/java/br/com/navita/patrimonio/configuracao/ResourceServerConfig.java
+++ src/main/java/br/com/navita/patrimonio/configuracao/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -31,7 +32,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http.requestMatchers()
             .and()
             .authorizeRequests()
-            .antMatchers("/publico/**", "/swagger-ui.html").permitAll()
+            .antMatchers(HttpMethod.POST,"/v1/usuarios").permitAll()
+            .antMatchers("/swagger-ui.html").permitAll()
             .antMatchers("/v1/**" ).authenticated();
     }
 
